84eecc83321833b1d5d89e0ca9b31f1afc14c454
cereceda1991 <cereceda1991@gmail.com>
2023-03-26 20:43:56 -0500

    my db

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,20 @@
-const express = require("express");
+const express = require('express');
+const cors = require('cors'); // Importamos el paquete CORS
 
-const routerUser=require('./routes/userRoutes.route')
-const routerRepair=require('./routes/repairRoutes.route')
+const routerUser = require('./routes/userRoutes.route');
+const routerRepair = require('./routes/repairRoutes.route');
 
-const app= express()
+const app = express(); // Creamos una nueva instancia de Express
 
-app.use(express.json());
+app.use(express.json()); // Añadimos el middleware para procesar datos en formato JSON
 
-app.use('/api/v1/repairs',routerRepair)
-app.use("/api/v1/users",routerUser)
+// Configuramos el middleware CORS para permitir solicitudes desde http://localhost:5173
+app.use(
+  cors({ origin: 'http://localhost:5173' })
+);
 
-module.exports=app;
+// Añadimos las rutas de usuarios y reparaciones
+app.use('/api/v1/repairs', routerRepair);
+app.use('/api/v1/users', routerUser);
+
+module.exports = app; // Exportamos la aplicación para ser usada en el archivo server.js
